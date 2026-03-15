3779988549ae561b7249819685cfc94c6a738f8e
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2026-03-15 17:40:29 +0200

    handle files and handle helper for configrations

---
+++ src/helpers/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, settingsconfigDict
+
+class Settings(BaseSettings):
+
+    OPENAI_API_KEY: str
+    app_name: str
+    app_version: str
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+
+def get_settings():
+    return Settings()
