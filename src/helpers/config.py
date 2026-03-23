3564b057fb8a6d9860a5015467b90d73db00f412
Youssef Abd-El-Ghfar Abd-El-Twab <91377316+youssefeissa18@users.noreply.github.com>
2026-03-23 17:54:36 +0200

    Install Docker and create db mongo and create scheme and make files for data come from docs

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -7,7 +7,8 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES : list
     FILE_MAX_SIZE_MB : int
     FILE_DEFAULT_CHUNK_SIZE : int
-
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
 
     class Config:
         env_file = ".env"
